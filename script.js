7c0a0c4ae80332ce5ca072102c1a9f7ece4e077a
Annie <Annie@gmail.com>
2023-10-26 17:49:34 +0530

    All files done ready to upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const searchFun = () =>{
+    let filter = document.getElementById('myInput').value.toUpperCase();
+
+    let myTable = document.getElementById('myTable');
+
+    let tr = myTable.getElementsByTagName('tr');
+
+    for(var i=0; i<tr.length; i++){
+        let td = tr[i].getElementsByTagName('td')[0];
+
+        if(td){
+            let textvalue = td.textContent || td.innerHTML;
+            if(textvalue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display = "";
+            }else{
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
